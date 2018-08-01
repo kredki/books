02e6ee07c7b0e15554b2fede87d42cb3d50ceb57
TL <31960565+kredki@users.noreply.github.com>
2018-08-01 09:16:52 +0200

    login and logout

--- web/src/main/java/pl/jstk/security/BasicConfiguration.java
+++ web/src/main/java/pl/jstk/security/BasicConfiguration.java
@@ -1,14 +1,12 @@
 package pl.jstk.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-
-import javax.sql.DataSource;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -16,9 +14,12 @@ public class BasicConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/webjars/**", "/css/*").permitAll()
+                .antMatchers("/", "/webjars/**", "/css/*", "/img/*").permitAll()
                 .anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().defaultSuccessUrl("/welcome")
-                .and().logout().permitAll();
+                .and()//.logout().logoutUrl("/logout").permitAll();
+                .logout().clearAuthentication(true).logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/logout.done").deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true);
     }
 
     @Override
